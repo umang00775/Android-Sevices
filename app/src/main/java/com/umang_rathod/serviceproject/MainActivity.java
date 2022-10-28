1fcb318ebe6e10aa760f508e69c6fe87715cd308
Umang Rathod <75923134+umang00775@users.noreply.github.com>
2022-10-28 12:59:26 +0530

    Works even if two or more apps are running

--- app/src/main/java/com/umang_rathod/serviceproject/MainActivity.java
+++ app/src/main/java/com/umang_rathod/serviceproject/MainActivity.java
@@ -2,21 +2,39 @@ package com.umang_rathod.serviceproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Only works when a single application is running
-        // Note: Also works in the background
-        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_ALARM_ALERT_URI);
-        player.setLooping(true);
-        player.start();
+
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // start onclick-lister
+        if (v == start){
+            startService(new Intent(this, AndroidService.class));
+        }
+        else if(v == stop){
+            stopService(new Intent(this, AndroidService.class));
+        }
     }
 }
